69134af25981ba33f7448340fd3be4fb2ae9b9d3
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-28 17:21:26 +0900

    add DownloadButton

--- host/App.js
+++ host/App.js
@@ -9,10 +9,11 @@ import PageButtons from './PageButtons'
 import Users from './Users'
 import Result from './Result'
 import Option from './Option'
+import DownloadButton from './DownloadButton'
 
 
-const mapStateToProps = ({ loading ,round ,maxround}) => ({
-  loading ,round ,maxround
+const mapStateToProps = ({ loading ,round ,maxround, results, participants, page}) => ({
+  loading ,round ,maxround, results, participants, page
 })
 
 class App extends Component {
@@ -27,7 +28,7 @@ class App extends Component {
   }
 
   render() {
-      const { loading ,round ,maxround} = this.props
+      const { loading ,round ,maxround, results, participants, page} = this.props
     if (loading) {
       return <div><p>ロード中です。</p></div>
     } else {
@@ -52,6 +53,23 @@ class App extends Component {
           <Result />
           <br />
           <Option />
+          <DownloadButton
+            fileName={"beauty_contest.csv"}
+            list={[
+              ["美人投票ゲーム"],
+              ["実験日", new Date()],
+              ["登録者数", Object.keys(participants).length],
+              ["ID"].concat(results.map(result => result.round + "ラウンド")),
+            ].concat(
+              Object.keys(participants).map(id => [id].concat(results.map(result => (id in result.participants)? result.participants[id].number : "未回答")))
+            ).concat([
+              ["平均値"].concat(results.map(result => Math.round(result.sum / result.inputs * 10) / 10))
+            ]).concat([
+              ["報酬基準値"].concat(results.map(result => Math.round(result.sum * 2 / result.inputs / 3 * 10) / 10))
+            ])}
+            style={{marginLeft: '2%'}}
+            disabled={page != "result"}
+          />
         </div>
       )
     }
